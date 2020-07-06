9384f2bc5b1c3bb7ef136205e7ad3907ef252300
Sam Kravitz <samkravitz99@gmail.com>
2020-07-05 20:22:47 -0400

    Simplify BEX instruction

--- cpu/tests/instruction_tests.cpp
+++ cpu/tests/instruction_tests.cpp
@@ -19,20 +19,20 @@ TEST_CASE("branch_exchange") {
     arm.execute(i1);
 
     REQUIRE(arm.registers.r15 == 0xbeefbeef); // contents of r9 moved to r15
-    REQUIRE(arm.get_mode() == THUMB); // because Rn (9) is odd
+    REQUIRE(arm.get_mode() == THUMB); // because Rn[0] is odd
 
-    arm.registers.r6 = 0xabcd;
+    arm.registers.r6 = 0xabcde;
 
     // BXHI 6 (BEX cond=HI Rn=6)
     arm_instruction i2 = 0b10000001001011111111111100010110;
     arm.execute(i2);
-    REQUIRE(arm.registers.r15 == 0xabcd); // contents of r6 moved to r15
-    REQUIRE(arm.get_mode() == ARM); // because Rn (6) is even
+    REQUIRE(arm.registers.r15 == 0xabcde); // contents of r6 moved to r15
+    REQUIRE(arm.get_mode() == ARM); // because Rn[0] is even
 
     // unrecognized condition (Rn = 15)
     arm_instruction i3 = 0b10000001001011111111111100011111;
     arm.execute(i3);
-    REQUIRE(arm.registers.r15 == 0xabcd); // contents of r15 unchanged
+    REQUIRE(arm.registers.r15 == 0xabcde); // contents of r15 unchanged
     REQUIRE(arm.get_state() == UND); // because Rn was 15
     REQUIRE(arm.get_mode() == ARM); // mode unchanged
 }

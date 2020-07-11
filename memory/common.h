e80a5104dba7dc69bf63a2d369d3cdfe19b093bb
Sam Kravitz <samkravitz99@gmail.com>
2020-07-11 18:59:36 -0400

    Add memory common.h

---
+++ memory/common.h
@@ -0,0 +1,14 @@
+/* discovery
+ * License: GPLv2
+ * See LICENSE.txt for full license text
+ * 
+ * FILE: common.h
+ * DATE: July 11, 2020
+ * DESCRIPTION: common values and types for gba memory
+ */
+#ifndef MEM_COMMON_H
+#define MEM_COMMON_H
+
+#define GBA_MEM_SIZE = 0xFFFFFFF;
+
+#endif // MEM_COMMON_H

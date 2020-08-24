416e665aa5b33d407bb18a6feb860831510b731f
Sam Kravitz <samkravitz99@gmail.com>
2020-08-24 15:07:00 -0400

    Add a few interrupt register addresses

--- src/common/common.h
+++ src/common/common.h
@@ -16,7 +16,10 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 
-const u32 REG_DISPCNT = 0x4000000;
+const u32 REG_DISPCNT  = 0x4000000;
 const u32 REG_KEYINPUT = 0x4000130;
+const u32 REG_IE       = 0x4000200; // interrupt enable register
+const u32 REG_IF       = 0x4000202; // interrupt request flags
+const u32 REG_IME      = 0x4000208; // master interrupt enable
 
 #endif // COMMON_H

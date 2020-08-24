981126e0e58754e4ecd53f7d4e76630f0c51e7ca
Sam Kravitz <samkravitz99@gmail.com>
2020-08-23 22:39:29 -0400

    Add video mode 4

--- gpu/gpu.cpp
+++ gpu/gpu.cpp
@@ -81,36 +81,37 @@ void GPU::draw_mode3() {
 
 // video mode 4 - bitmap mode
 void GPU::draw_mode4() {
-    // u8 current_pixel; // in mode 3 each pixel uses 2 bytes
-    // u8 r;
-    // u8 g;
-    // u8 b;
-    // u8 alpha = 0;
-    // u32 *pixels = new u32[SCREEN_WIDTH * SCREEN_HEIGHT]; // array representing each pixel on the screen
+    u8 pallette_index; // in mode 4 each pixel uses 1 byte 
+    u32 current_pixel; // the color located at pallette_ram[pallette_index]
+    u8 r;
+    u8 g;
+    u8 b;
+    u8 alpha = 0;
+    u32 *pixels = new u32[SCREEN_WIDTH * SCREEN_HEIGHT]; // array representing each pixel on the screen
 
-    // for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; ++i) {
-    //     current_pixel = mem->read_u16(MEM_VRAM_START + (2 * i)); // multiply i * 2 b/c each pixel is 2 bytes
-    //     r = five_bits_to_eight(current_pixel & 0b11111);
-    //     g = five_bits_to_eight((current_pixel >> 5) & 0b11111);
-    //     b = five_bits_to_eight((current_pixel >> 10) & 0b11111);
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; ++i) {
+        pallette_index = mem->read_u8(MEM_VRAM_START + i);
+        current_pixel = mem->read_u32(MEM_PALETTE_RAM_START + (pallette_index * sizeof(u16)));
+        r = five_bits_to_eight(current_pixel & 0b11111);
+        g = five_bits_to_eight((current_pixel >> 5) & 0b11111);
+        b = five_bits_to_eight((current_pixel >> 10) & 0b11111);
 
-    //     // add current pixel in argb format to pixel array
-    //     pixels[i] = alpha;
-    //     pixels[i] <<= 8;
-    //     pixels[i] |= b;
-    //     pixels[i] <<= 8;
-    //     pixels[i] |= g;
-    //     pixels[i] <<= 8;
-    //     pixels[i] |= r;
-    // }   
+        // add current pixel in argb format to pixel array
+        pixels[i] = alpha;
+        pixels[i] <<= 8;
+        pixels[i] |= r;
+        pixels[i] <<= 8;
+        pixels[i] |= g;
+        pixels[i] <<= 8;
+        pixels[i] |= b;
+    }   
 
-    // SDL_UpdateTexture(texture, NULL, pixels, SCREEN_WIDTH * sizeof(u32));
-    // SDL_RenderClear(renderer);
-    // SDL_RenderCopy(renderer, texture, NULL, NULL);
-    // SDL_RenderPresent(renderer);
+    SDL_UpdateTexture(texture, NULL, pixels, SCREEN_WIDTH * sizeof(u32));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 
-    // delete[] pixels;
-    std::cout << "Video mode 4!\n";
+    delete[] pixels;
 }
 
 // given a range of 0-31 return a range of 0-255

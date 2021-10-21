f1b185ccdd67d2be5186ec143595fc7fb8e2b25f
Noah Bennett <nbennett.320@gmail.com>
2021-10-21 14:33:36 -0400

    wip: audiobuffer in apu 2

--- src/APU.cpp
+++ src/APU.cpp
@@ -23,7 +23,7 @@
 APU::APU(Memory *mem, Scheduler *scheduler):
 mem(mem),
 scheduler(scheduler),
-audio_buffer(this->NUM_SAMPLES)
+audio_buffer(new CircularBuffer<s16>(this->NUM_SAMPLES))
 {
 	SDL_Init(SDL_INIT_AUDIO);
 	std::cout<<"driver: "<<SDL_GetCurrentAudioDriver()<<std::endl;
